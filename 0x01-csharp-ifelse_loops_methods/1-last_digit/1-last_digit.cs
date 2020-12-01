38ec55aad378a7b32532e629f7cb797fa7ae706b
MenacingManatee <brett.p.davis101@gmail.com>
2020-12-01 04:56:10 -0600

    Adds tasks 0 and 1

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int ld = number % 10;
+        if (ld > 5)
+            Console.WriteLine("The last digit of {0} is {1} and is greater than 5", number, ld);
+        else if (ld < 6 && ld != 0)
+            Console.WriteLine("The last digit of {0} is {1} and is less than 6 and not 0", number, ld);
+        else
+            Console.WriteLine("The last digit of {0} is {1} and is 0", number, ld);
+    }
+}
