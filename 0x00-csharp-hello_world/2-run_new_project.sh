7b0fd1d46fd109db31a9c109bd1bdf7948ddb64e
MenacingManatee <brett.p.davis101@gmail.com>
2020-11-30 18:36:34 -0600

    removed unnecessary flags and path

--- 0x00-csharp-hello_world/2-run_new_project.sh
+++ 0x00-csharp-hello_world/2-run_new_project.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # runs a new C# project inside a folder
-dotnet new console -n 2-new_project
-dotnet build --no-restore 2-new_project/2-new_project.csproj
-dotnet run --project 2-new_project/2-new_project.csproj
+dotnet new console -o 2-new_project
+dotnet build 2-new_project/
+dotnet run --project 2-new_project/
