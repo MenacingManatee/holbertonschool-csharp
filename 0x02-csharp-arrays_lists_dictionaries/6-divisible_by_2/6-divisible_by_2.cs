0f97f089edeef4ed903293622d7ff0a2f35c6b23
MenacingManatee <brett.p.davis101@gmail.com>
2020-12-02 06:08:59 -0600

    Adds tasks 5-7. Bugs in 7 if duplicates in list

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> l = new List<bool>();
+        int i;
+        for (i = 0; i < myList.Count; i++)
+        {
+            l.Add(myList[i] % 2 == 0);
+        }
+        return l;
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> myList = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+        foreach (int i in myList)
+            Console.WriteLine(i);
+
+        Console.WriteLine("----------");
+
+        List.DeleteAt(myList, 2);
+
+        foreach (int i in myList)
+            Console.WriteLine(i);
+
+        Console.WriteLine("----------");
+
+        List.DeleteAt(myList, 5);
+
+        foreach (int i in myList)
+            Console.WriteLine(i);
+    }
+}
