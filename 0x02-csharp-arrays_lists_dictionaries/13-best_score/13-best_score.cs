f95a7bf41aa2ca06e07c78b9744bcab5cf32ee85
MenacingManatee <brett.p.davis101@gmail.com>
2020-12-02 07:01:35 -0600

    Task 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string bk = "None";
+        int big = -1;
+        foreach (KeyValuePair<string, int> e in myList)
+        {
+            if (e.Value >= big)
+            {
+                big = e.Value;
+                bk = e.Key;
+            }
+        }
+        return bk;
+    }
+}
