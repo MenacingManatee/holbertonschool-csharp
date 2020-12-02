cf5550a739e2986677a05263a111d97c6bd7b009
MenacingManatee <brett.p.davis101@gmail.com>
2020-12-02 02:28:18 -0600

    adds blank readme and tasks 0-2

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else if (size == 0)
+        {
+            Console.Write("\n");
+            return new int[0];
+        }
+        int[] res = new int[size];
+        string sep = " ";
+        for (int count = 0; count < size; count++)
+        {
+            if (count + 1 == size)
+                sep = "\n";
+            res[count] = count;
+            Console.Write("{0}{1}", count, sep);
+        }
+        return res;
+    }
+}
