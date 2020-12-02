5d3f4fa858117e3a934e335af0fe778498ff3ec5
MenacingManatee <brett.p.davis101@gmail.com>
2020-12-02 06:52:43 -0600

    Converts keys to list to sort

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var l = myDict.Keys.ToList();
+        l.Sort();
+        foreach (var k in l)
+        {
+            Console.WriteLine("{0}: {1}", k, myDict[k]);
+        }
+    }
+}
