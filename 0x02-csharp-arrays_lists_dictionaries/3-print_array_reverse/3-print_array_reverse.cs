bb28ded7d2a06709f16171454f48a0f86cc8989b
MenacingManatee <brett.p.davis101@gmail.com>
2020-12-02 05:29:52 -0600

    Adds 3. Running into errors on 4, circle back ltr

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.Write("\n");
+            return;
+        }
+        int i = array.Length - 1;
+        string sep = " ";
+        for (; i >= 0; i--)
+        {
+            if (i == 0)
+                sep = "\n";
+            Console.Write("{0}{1}", array[i], sep);
+        }
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List<int> l;
+        int i;
+        string sep = " ";
+        for (i = 0; i < size; i++)
+        {
+            if (i == size - 1)
+                sep = "\n";
+            l.Add(i);
+            Console.Write("{0}{1}", i, sep);
+        }
+        return l;
+    }
+}
