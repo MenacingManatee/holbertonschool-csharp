2cdc58218269287dc9d1c1748524cec0ffa61ec4
MenacingManatee <brett.p.davis101@gmail.com>
2020-12-02 18:47:12 -0600

    Task 14, and prototype files for 15 and 100

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] m = new int[5, 5];
+            int i;
+            int j;
+            string sep = " ";
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    if (j == 4)
+                        sep = "\n";
+                    if (i == 2 && j == 2)
+                        m[i, j] = 1;
+                    else
+                        m[i, j] = 0;
+                    Console.Write("{0}{1}", m[i, j], sep);
+                }
+                sep = " ";
+            }
+        }
+    }
+}
