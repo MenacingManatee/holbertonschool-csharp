ccdf2b3884b3cfbc248c7447206ce7487bebbd77
MenacingManatee <brett.p.davis101@gmail.com>
2020-12-02 20:34:41 -0600

    Task 100

--- 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -6,7 +6,21 @@ namespace _100_jagged_array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[][] ja = new int[3][];
+            ja[0] = new int[] {0, 1, 2, 3};
+            ja[1] = new int[] {0, 1, 2, 3, 4, 5, 6};
+            ja[2] = new int[] {0, 1};
+            string sep = " ";
+            for (int i = 0; i < ja.Length; i++)
+            {
+                for (int j = 0; j < ja[i].Length; j++)
+                {
+                    if (j == ja[i].Length - 1)
+                        sep = "\n";
+                    Console.Write("{0}{1}", ja[i][j], sep);
+                }
+                sep = " ";
+            }
         }
     }
 }
