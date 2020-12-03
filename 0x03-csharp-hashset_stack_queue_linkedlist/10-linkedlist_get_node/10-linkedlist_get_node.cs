490d1c5f3033277c2e5bb6f2717773a4942038f1
MenacingManatee <brett.p.davis101@gmail.com>
2020-12-03 05:11:35 -0600

    Tasks 9-13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        var node = myLList.First;
+
+        for (int count = 0; count < myLList.Count && node != null; count++, node = node.Next)
+        {
+            if (count == n)
+            {
+                return node.Value;
+            }
+        }
+        return 0;
+    }
+}
