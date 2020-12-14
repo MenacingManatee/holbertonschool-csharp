1af0ee5a5e6f620966804f34cd0ee494c3bb9c4f
MenacingManatee <brett.p.davis101@gmail.com>
2020-12-14 03:27:22 -0600

    Adds all files for mandatory tasks

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> l = new List<int>();
+        int res = 0;
+        for (int count = 0; count < listLength; count++)
+        {
+            try {
+                res = list1[count] / list2[count];
+                l.Add(res);
+            } catch (DivideByZeroException) {
+                res = 0;
+                Console.WriteLine("Cannot divide by zero");
+                l.Add(res);
+            } catch (ArgumentOutOfRangeException) {
+                Console.WriteLine("Out of range");
+                return l;
+            }
+            res = 0;
+        }
+        return l;
+    }
+}
