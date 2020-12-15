d0b6f1d76897fd2d8936a59b692bcbc06c98f7e9
MenacingManatee <brett.p.davis101@gmail.com>
2020-12-14 19:01:47 -0600

    Simplified code to make finding edge cases easier

--- 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -6,21 +6,17 @@ class List
     public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
     {
         List<int> l = new List<int>();
-        int res = 0;
+
         for (int count = 0; count < listLength; count++)
         {
             try {
-                res = list1[count] / list2[count];
-                l.Add(res);
+                l.Add(list1[count] / list2[count]);
             } catch (DivideByZeroException) {
-                res = 0;
                 Console.WriteLine("Cannot divide by zero");
-                l.Add(res);
+                l.Add(0);
             } catch (ArgumentOutOfRangeException) {
                 Console.WriteLine("Out of range");
-                return l;
             }
-            res = 0;
         }
         return l;
     }
