081f1e17c68806e907ebe68425919bc717cadff1
MenacingManatee <brett.p.davis101@gmail.com>
2020-12-15 07:21:30 -0600

    Tasking 5, saving progress. Tasks 0-4

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        public int health;
+
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
