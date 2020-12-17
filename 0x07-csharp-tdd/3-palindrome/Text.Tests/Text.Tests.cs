2f3c3e60e882aeb643cea064fefc7ff6d4fc7290
MenacingManatee <brett.p.davis101@gmail.com>
2020-12-17 07:31:25 -0600

    Task 3

--- 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -1,6 +1,6 @@
 using NUnit.Framework;
 
-namespace Tests
+namespace Text.Tests
 {
     public class Tests
     {
@@ -12,7 +12,27 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            Assert.IsTrue(Text.Str.IsPalindrome("level"));
+        }
+        [Test]
+        public void Test_caps()
+        {
+            Assert.IsTrue(Text.Str.IsPalindrome("Racecar"));
+        }
+        [Test]
+        public void Test_punc()
+        {
+            Assert.IsTrue(Text.Str.IsPalindrome("A man, a plan, a canal: Panama."));
+        }
+        [Test]
+        public void Test_fail()
+        {
+            Assert.IsFalse(Text.Str.IsPalindrome("Race"));
+        }
+        [Test]
+        public void Test_empty()
+        {
+            Assert.IsTrue(Text.Str.IsPalindrome(""));
         }
     }
 }
--- 0x07-csharp-tdd/3-palindrome/Text/Text.cs
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Text
 {
-    public class Class1
+    public class Str
     {
+        public static bool IsPalindrome(string s)
+        {
+            if (s == null)
+                return (false);
+            if (s == "")
+                return (true);
+            string n = s.ToLower();
+            n = Regex.Replace(n, "[^a-z]*", "");
+            char[] tmp = n.ToCharArray();
+            Array.Reverse(tmp);
+            string tmp2 = new string(tmp);
+            return (n == tmp2);
+        }
     }
 }
