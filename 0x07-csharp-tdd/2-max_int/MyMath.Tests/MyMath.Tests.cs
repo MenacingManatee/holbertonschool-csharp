b436aebc203e01a4e7b575864cbeb297c0f3405e
MenacingManatee <brett.p.davis101@gmail.com>
2020-12-17 07:01:00 -0600

    Task 2

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Tests
 {
@@ -10,9 +11,52 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void Test_sorted()
         {
-            Assert.Pass();
+            var l = new List<int>()
+            {1, 2, 3, 4, 5, 6, 7, 8};
+            int mx = MyMath.Operations.Max(l);
+            Assert.AreEqual(mx, 8);
+        }
+        [Test]
+        public void Test_sorted_rev()
+        {
+            var l = new List<int>()
+            {9, 8, 7, 6, 5, 4, 3, 2, 1};
+            int mx = MyMath.Operations.Max(l);
+            Assert.AreEqual(mx, 9);
+        }
+        [Test]
+        public void Test_unsorted()
+        {
+            var l = new List<int>()
+            {1, 4, 3, 7, 5, 6, 2};
+            int mx = MyMath.Operations.Max(l);
+            Assert.AreEqual(mx, 7);
+        }
+        [Test]
+        public void Test_double_digits()
+        {
+            var l = new List<int>()
+            {1, 12, 32, 24, 51, 56, 57, 18};
+            int mx = MyMath.Operations.Max(l);
+            Assert.AreEqual(mx, 57);
+        }
+        [Test]
+        public void Test_neg()
+        {
+            var l = new List<int>()
+            {-11, -22, -13, -66, -5, -93};
+            int mx = MyMath.Operations.Max(l);
+            Assert.AreEqual(mx, -5);
+        }
+        [Test]
+        public void Test_empty()
+        {
+            var l = new List<int>()
+            {};
+            int mx = MyMath.Operations.Max(l);
+            Assert.AreEqual(mx, 0);
         }
     }
 }
--- 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -1,8 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyMath
 {
-    public class Class1
+    /// <summary>
+    /// Operations class for math operations
+    /// </summary>
+    public class Operations
     {
+        /// <summary>
+        /// Finds the largest in in a list
+        /// </summary>
+        public static int Max(List<int> nums)
+        {
+            if (nums.Count == 0)
+                return (0);
+            List<int> l = new List<int>(nums);
+            l.Sort();
+            return (l[l.Count - 1]);
+        }
     }
 }
