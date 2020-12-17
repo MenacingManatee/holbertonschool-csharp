74bee0b930701810ffe1c9019789f7b967be7ac4
MenacingManatee <brett.p.davis101@gmail.com>
2020-12-17 08:53:37 -0600

    Task 5

--- 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -1,7 +1,8 @@
 using NUnit.Framework;
 
-namespace Tests
+namespace Text.Tests
 {
+    [TestFixture]
     public class Tests
     {
         [SetUp]
@@ -10,9 +11,28 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void Test_empty()
         {
-            Assert.Pass();
+            int res = Text.Str.CamelCase("");
+            Assert.AreEqual(0, res);
+        }
+        [Test]
+        public void Test_null()
+        {
+            int res = Text.Str.CamelCase(null);
+            Assert.AreEqual(0, res);
+        }
+        [Test]
+        public void Test_one()
+        {
+            int res = Text.Str.CamelCase("test");
+            Assert.AreEqual(1, res);
+        }
+        [Test]
+        public void Test_many()
+        {
+            int res = Text.Str.CamelCase("theQuickBrownFox");
+            Assert.AreEqual(4, res);
         }
     }
 }
--- 0x07-csharp-tdd/5-camelcase/Text/Text.cs
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Text
 {
-    public class Class1
+    /// <summary>
+    /// Str class for string methods
+    /// </summary>
+    public class Str
     {
+        /// <summary>
+        /// Determines the number of words in a string
+        /// </summary>
+        public static int CamelCase(string s)
+        {
+            if (s == null || s.Length == 0)
+                return (0);
+            string n = Regex.Replace(s, "[^A-Z]", "");
+            return (1 + n.Length);
+        }
     }
 }
