a583a6b21dbdeb38ef3facbfc1fde3a90e3fdf58
MenacingManatee <brett.p.davis101@gmail.com>
2021-01-12 15:32:27 -0600

    Task 3 and adds XML documentation line to files

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -2,11 +2,25 @@
 
 namespace _3_magnitude
 {
-    class Program
+    /// <summary>
+    /// VectorMath class
+    /// </summary>
+    class VectorMath
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Finds the magnitude of a 2d or 3d vector
+        /// </summary>
+        public static double Magnitude(double[] vector)
         {
-            Console.WriteLine("Hello World!");
+            if (vector.Length == 2)
+            {
+                return (Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)), 1));
+            }
+            if (vector.Length == 3)
+            {
+                return (Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)), 1));
+            }
+            return (-1);
         }
     }
 }
---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3_magnitude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vector_2D = new double[] {3, 9};
+            double[] vector_3D = new double[] {7, -3, -9};
+            Console.WriteLine(VectorMath.Magnitude(vector_2D));
+            Console.WriteLine(VectorMath.Magnitude(vector_3D));
+        }
+    }
+}
