ea9109e699f62daaa553e27a2c1e2f1c9a4c8e4f
MenacingManatee <brett.p.davis101@gmail.com>
2021-01-19 23:50:48 -0600

    Fixed result array in oddly shaped arrays

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-main.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-main.cs
@@ -11,6 +11,8 @@ namespace _18_matrix_matrix_mul
             double[,] matrix_2D_2 = new double[,] { {1, 7}, {-8, -2} };
             double[,] matrix_3D_1 = new double[,] { {14, -3, 0}, {-11, -5, 3}, {2, -9, 13} };
             double[,] matrix_3D_2 = new double[,] { {6, 16, 21}, {5, 2, 0}, {1, 3, 7} };
+            double[,] matrix_3D_3 = new double[,] { {7, 8}, {9, 10}, {11, 12} };
+            double[,] matrix_2D_3 = new double[,] { {1, 2, 3}, {4, 5, 6} };
             double[,] tmp;
 
             tmp = MatrixMath.Multiply(matrix_2D_1, matrix_2D_2);
@@ -32,6 +34,24 @@ namespace _18_matrix_matrix_mul
             }
             Console.WriteLine("}");
             tmp = MatrixMath.Multiply(matrix_3D_1, matrix_3D_2);
+            Console.Write("{");
+            for (var i = 0; i < tmp.GetLength(0); i++)
+            {
+                Console.Write("{");
+                for (var j = 0; j < tmp.GetLength(1); j++)
+                {
+                    Console.Write(tmp[i, j]);
+                    if (j != tmp.GetLength(1) - 1)
+                        Console.Write(", ");
+                }
+                Console.Write("}");
+                if (i != tmp.GetLength(0) - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine("}");
+            tmp = MatrixMath.Multiply(matrix_2D_3, matrix_3D_3);
             Console.Write("{");
             for (var i = 0; i < tmp.GetLength(0); i++)
             {
