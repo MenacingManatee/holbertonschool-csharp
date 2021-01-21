d353e34eb78056b95281df6149a43b4ffe87e7dd
MenacingManatee <brett.p.davis101@gmail.com>
2021-01-21 02:59:31 -0600

    Tasks 24-26

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace _28_determinant
+/// <summary>
+/// MatrixMath class
+/// </summary>
+class MatrixMath
 {
-    class Program
+    /// <summary>
+    /// Transposes a matrix
+    /// </summary>
+    public static double Determinant(double[,] matrix)
     {
-        static void Main(string[] args)
+        double res;
+
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
         {
-            Console.WriteLine("Hello World!");
+            return ((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]));
         }
+        if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+        {
+            res = matrix[0, 0] * ((matrix[1, 1] * matrix[2, 2]) - (matrix[2, 1] * matrix[1, 2]));
+            res -= matrix[0, 1] * ((matrix[1, 0] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 0]));
+            res += matrix[0, 2] * ((matrix[1, 0] * matrix[2, 1]) - (matrix[1, 1] * matrix[2, 0]));
+            return (res);
+        }
+        return (-1);
     }
 }
---
+++ 0x09-csharp-linear_algebra/28-determinant/28-main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _28_determinant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] matrix_2 = new double[,] { {2, 2}, {-9, 4} };
+            double[,] matrix_3 = new double[,] { {-4, 9, 0}, {1, -2, 1}, {3, -4, 2} };
+            double[,] matrix_e = new double[,] {{}};
+
+            Console.WriteLine(MatrixMath.Determinant(matrix_2));
+            Console.WriteLine(MatrixMath.Determinant(matrix_3));
+            Console.WriteLine(MatrixMath.Determinant(matrix_e));
+            
+        }
+    }
+}
