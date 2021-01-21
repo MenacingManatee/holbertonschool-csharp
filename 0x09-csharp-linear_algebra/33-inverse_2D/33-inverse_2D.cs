7cf60d8a5384b596413a4e8c777345a81c84b2dd
MenacingManatee <brett.p.davis101@gmail.com>
2021-01-21 03:44:15 -0600

    Tasks 29 and 31

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace _33_inverse_2D
+/// <summary>
+/// MatrixMath class
+/// </summary>
+class MatrixMath
 {
-    class Program
+    /// <summary>
+    /// Transposes a matrix
+    /// </summary>
+    public static double[,] Transpose(double[,] matrix)
     {
-        static void Main(string[] args)
+        double[,] res = new double[2, 2];
+
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
         {
-            Console.WriteLine("Hello World!");
+            double determinant = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+            if (determinant == 0)
+                return (new double[,] {{-1}});
+            res[0, 0] = Math.Round(matrix[1, 1] * (1 / determinant), 2);
+            res[1, 1] = Math.Round(matrix[0, 0]* (1 / determinant), 2);
+            res[0, 1] = Math.Round((matrix[0, 1] * -1) * (1 / determinant), 2);
+            res[1, 0] = Math.Round((matrix[1, 0] * -1) * (1 / determinant), 2);
+            return (res);
         }
+        return (new double[,] {{-1}});
     }
 }
---
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-main.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _33_inverse_2D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] matrix_1 = new double[,] { {1, 2}, {3, 4} };
+            double[,] matrix_2 = new double[,] { {3, 6}, {9, 12}, {15, 18} };
+            double[,] tmp;
+
+            tmp = MatrixMath.Transpose(matrix_1);
+            PrintMatrix(tmp);
+            tmp = MatrixMath.Transpose(matrix_2);
+            PrintMatrix(tmp);
+            
+        }
+        static void PrintMatrix(double[,] matrix)
+        {
+            Console.Write("{");
+            for (var i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.Write("{");
+                for (var j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j]);
+                    if (j != matrix.GetLength(1) - 1)
+                        Console.Write(", ");
+                }
+                Console.Write("}");
+                if (i != matrix.GetLength(0) - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine("}");
+        }
+    }
+}
