80ad0f587b5420636aafc2123a0db959e2083516
MenacingManatee <brett.p.davis101@gmail.com>
2021-01-21 04:17:30 -0600

    Rounds to nearest hundredth instead of tenth

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -12,11 +12,11 @@ class VectorMath
     {
         if (vector.Length == 2)
         {
-            return (Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)), 1));
+            return (Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)), 2));
         }
         if (vector.Length == 3)
         {
-            return (Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)), 1));
+            return (Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)), 2));
         }
         return (-1);
     }
