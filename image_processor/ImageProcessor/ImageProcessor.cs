65a2f61a849eac94504e5da20d13657a8c36655d
Brett Davis <brett.p.davis101@gmail.com>
2021-02-10 00:43:16 -0600

    Inverts an image

---
+++ image_processor/ImageProcessor/ImageProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+class ImageProcessor
+{
+    public static void Inverse(string[] filenames)
+    {
+        foreach (var filename in filenames) {
+            Console.Write(filename);
+            using (var image = new Bitmap(System.Drawing.Image.FromFile(filename)))
+            {
+                ImageFormat format = image.RawFormat;
+                for (int y = 0; y < image.Height; y++)
+                {
+                    for (int x = 0; x < image.Width; x++)
+                    {
+                        Color n = image.GetPixel(x, y);
+                        n = Color.FromArgb(255, (255 - n.R), (255 - n.G), (255 - n.B));
+                        image.SetPixel(x, y, n);
+                    }
+                }
+                string fname = Path.GetFileName(filename);
+                string[] fname_split = fname.Split('.');
+                image.Save(fname_split[0] + "_inverse." + fname_split[1], format);
+            }
+        }
+    }
+}
