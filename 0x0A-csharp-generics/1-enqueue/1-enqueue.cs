192d0c1583d56506b2adb35500f3a092bb1e67e3
Brett Davis <brett.p.davis101@gmail.com>
2021-03-22 18:19:08 -0500

    Tasks 0-1

---
+++ 0x0A-csharp-generics/1-enqueue/1-enqueue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Queue<T>
+{
+    public Node head = null;
+    public Node tail = null;
+    public int count = 0;
+    public string CheckType()
+    {
+        return (typeof(T).ToString());
+    }
+    public void Enqueue(T val) {
+        Node n = new Node(val);
+
+        if (head == null) {
+            head = n;
+        }
+        tail = n;
+        count++;
+    }
+    public int Count()
+    {
+        return (count);
+    }
+    public class Node
+    {
+        public T value;
+        public Node next = null;
+
+        public Node(T val)
+        {
+            value = val;
+        }
+    }
+}
