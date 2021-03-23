d96acba10e4f7d24f5702beb78942348580b2e5f
Brett Davis <brett.p.davis101@gmail.com>
2021-03-23 15:30:11 -0500

    Updates XML documentation

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,51 +1,51 @@
 ﻿using System;
 
-// <summary> Abstract base class </summary>
+/// <summary> Abstract base class </summary>
 public abstract class Base
 {
-    // <summary> name string </summary>
+    /// <summary> name string </summary>
     public string name {get; set;}
 
-    // <summary> Override for ToString method </summary>
+    /// <summary> Override for ToString method </summary>
     public override string ToString()
     {
         return (String.Format("{0} is a {1}", name, this.GetType().ToString()));
     }
 }
 
-// <summary> Interactive interface </summary>
-interface IInteractive {
-    // <summary> Interactions </summary>
+/// <summary> Interactive interface </summary>
+public interface IInteractive {
+    /// <summary> Interactions </summary>
     void Interact();
 }
 
-// <summary> Breakable interface </summary>
-interface IBreakable {
-    // <summary> Item durability tracker </summary>
+/// <summary> Breakable interface </summary>
+public interface IBreakable {
+    /// <summary> Item durability tracker </summary>
     int durability {get; set;}
-    // <summary> Break an item </summary>
+    /// <summary> Break an item </summary>
     void Break();
 }
 
-// <summary> Collectable interface </summary>
-interface ICollectable {
-    // <summary> Is the object collected </summary>
+/// <summary> Collectable interface </summary>
+public interface ICollectable {
+    /// <summary> Is the object collected </summary>
     bool isCollected {get; set;}
-    // <summary> Collect the object </summary>
+    /// <summary> Collect the object </summary>
     void Collect();
 }
 
-// <summary> Inherits from Base and all 3 interfaces </summary>
+/// <summary> Inherits from Base and all 3 interfaces </summary>
 class TestObject : Base, IInteractive, IBreakable, ICollectable {
-    // <summary> item durability tracker </summary>
+    /// <summary> item durability tracker </summary>
     public int durability {get; set; }
-    // <summary> item collection tracker </summary>
+    /// <summary> item collection tracker </summary>
     public bool isCollected {get; set; }
 
-    // <summary> When object interacts with something </summary>
+    /// <summary> When object interacts with something </summary>
     public void Interact() {}
-    // <summary> When object breaks </summary>
+    /// <summary> When object breaks </summary>
     public void Break() {}
-    // <summary> When object collects something </summary>
+    /// <summary> When object collects something </summary>
     public void Collect() {}
 }
