46aabd1a1cc97c8fd5b763247d73bfb451b17c44
Brett Davis <brett.p.davis101@gmail.com>
2021-03-23 15:17:44 -0500

    Documents task 1

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -3,24 +3,34 @@
 // <summary> Abstract base class </summary>
 abstract class Base
 {
+    // <summary> name string </summary>
     public string name {get; set;}
 
+    // <summary> Override for ToString method </summary>
     public override string ToString()
     {
         return (String.Format("{0} is a {1}", name, this.GetType().ToString()));
     }
 }
+
+// <summary> Interactive interface </summary>
 interface IInteractive {
     void Interact();
 }
+
+// <summary> Breakable interface </summary>
 interface IBreakable {
     int durability {get; set;}
     void Break();
 }
+
+// <summary> Collectable interface </summary>
 interface ICollectable {
     bool isCollected {get; set;}
     void Collect();
 }
+
+// <summary> Inherits from Base and all 3 interfaces </summary>
 class TestObject : Base, IInteractive, IBreakable, ICollectable {
     public int durability {get; set; }
     public bool isCollected {get; set; }
