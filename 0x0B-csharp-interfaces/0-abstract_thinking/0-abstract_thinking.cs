21ef1531eade446786602dafc770000c157e9e90
Brett Davis <brett.p.davis101@gmail.com>
2021-03-23 06:52:42 -0500

    Task 0, error cs8701 on task 1 prevents testing

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+abstract class Base
+{
+    public string name;
+
+    public override string ToString()
+    {
+        return (String.Format("{0} is a {1}", name, this.GetType().ToString()));
+    }
+}
---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+abstract class Base
+{
+    public string name;
+
+    public override string ToString()
+    {
+        return (String.Format("{0} is a {1}", name, this.GetType().ToString()));
+    }
+}
+interface IInteractive {
+    void Interact() {
+        //empty method
+    }
+}
+interface IBreakable {
+    int durability {get; set;}
+    void Break() {
+        //empty method
+    }
+}
+interface ICollectable {
+    bool isCollected {get; set;}
+    void Collect() {
+        // empty method
+    }
+}
+class TestObject : Base, IInteractive, IBreakable, ICollectable {
+    void ICollectable.Collect() {}
+    public int durability {get; set; }
+    public bool isCollected {get; set; }
+}
