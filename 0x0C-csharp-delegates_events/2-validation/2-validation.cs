66f1ee8ffc83c06976ac2d3fb13ede8767f3fa40
Brett Davis <brett.p.davis101@gmail.com>
2021-03-24 22:18:57 -0500

    Task 2

---
+++ 0x0C-csharp-delegates_events/2-validation/2-validation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+    public Player(string name="Player", float maxHp=100f) {
+        if (maxHp <= 0f) {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+            this.maxHp = maxHp;
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+    public void PrintHealth() {
+        Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
+    }
+    public void TakeDamage(float damage) {
+        if (damage <= 0f) {
+            damage = 0f;
+        }
+        float newHp = hp - damage;
+        ValidateHP(newHp);
+        Console.WriteLine("{0} takes {1} damage!", name, damage);
+    }
+    public void HealDamage(float heal) {
+        if (heal <= 0f) {
+            heal = 0f;
+        }
+        float newHp = hp + heal;
+        ValidateHP(newHp);
+        Console.WriteLine("{0} heals {1} HP!", name, heal);
+    }
+    public void ValidateHP(float newHp) {
+        if (newHp <= 0) {
+            hp = 0;
+        }
+        else if (newHp >= maxHp) {
+            hp = maxHp;
+        }
+        else {
+            hp = newHp;
+        }
+    }
+}
