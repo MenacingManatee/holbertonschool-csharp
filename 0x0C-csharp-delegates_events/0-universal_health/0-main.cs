ba3e7683ab758c1c8beadeca41565df44e520cd3
Brett Davis <brett.p.davis101@gmail.com>
2021-03-24 21:57:58 -0500

    Task 0

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player player1 = new Player("Electric Mouse", 500f);
+        Player player2 = new Player("Water Turtle");
+
+        player1.PrintHealth();
+        player2.PrintHealth();
+        Player player3 = new Player("Turducken", 0f);
+        player3.PrintHealth();
+    }
+}
---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+    public Player(string name="Player", float maxHp=100f) {
+        if (maxHp <= 0f) {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+            this.maxHp = maxHp;
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+    public void PrintHealth() {
+        Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
+    }
+}
