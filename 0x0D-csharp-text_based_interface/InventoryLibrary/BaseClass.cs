a398b3e9d68f28eecfa0d1a8c9e79e4e2fb69b14
Brett Davis <brett.p.davis101@gmail.com>
2021-03-29 17:54:19 -0500

    Task 0

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// <summary> Base Class to be inherited from </summary>
+class BaseClass
+{
+    /// <summary> ID of object </summary>
+    public string id { get; set; }
+    /// <summary> Creation datetime of object </summary>
+    public DateTime date_created { get; set; }
+    /// <summary> last updated datetime of object </summary>
+    public DateTime date_updated { get; set; }
+}
