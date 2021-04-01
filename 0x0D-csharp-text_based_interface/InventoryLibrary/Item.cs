000fb1c183ff537d633f71151ef83f73697611e6
Brett Davis <brett.p.davis101@gmail.com>
2021-04-01 06:13:45 -0500

    Adds more tests

--- 0x0D-csharp-text_based_interface/InventoryLibrary/Item.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/Item.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Linq;
 
 /// <summary> Item Class </summary>
 public class Item : BaseClass
@@ -17,15 +18,16 @@ public class Item : BaseClass
     /// <summary> Optional price </summary>
     public float price { get; set; }
     /// <summary> Optional list of tags </summary>
-    public string[] tags { get; set; }
+    public List<string> tags { get; set; }
     /// <summary> Item constructor </summary>
-    public Item(string name, string description=null, float price=-1, string[] tags=null) {
+    public Item(string name, string description=null, float price=-1, List<string> tags=null) {
         this.name = name;
         this.description = description;
         if (price != -1) {
             this.price = (float)Math.Round(price * 100f) / 100f;
         }
-        this.tags = tags;
+        if (tags != null)
+            this.tags = tags.ToList<string>();
     }
     /// <summary> String override </summary>
     public override string ToString() {
